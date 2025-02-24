ce0fbe8e6aa56990da7643d928f8825d00581088
Ivan Krstev <76234294+ivankrstev@users.noreply.github.com>
2025-02-24 23:10:46 +0100

    implement lexer for tokenizing input strings

--- src/main/java/com/spella/lexer/Lexer.java
+++ src/main/java/com/spella/lexer/Lexer.java
@@ -4,48 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lexer {
-
     /*
         Takes stream of characters as input and turns them into
         meaningful tokens, that are then processed further by parser
      */
     public List<Token> tokenize(String input) {
-
         List<Token> tokens = new ArrayList<>();
         int cursor = 0;
         int line = 0;
         int column = 0;
 
         while (cursor < input.length()) {
-
-            if (input.charAt(cursor) == ' ') {
-                cursor++; column++;
+            char c = input.charAt(cursor);
+            if (Character.isWhitespace(c)) {
+                if (c == '\n') {
+                    line++;
+                    column = 0;
+                } else column++;
+                cursor++;
+                continue;
             }
-            if (input.charAt(cursor) == '\n') {
-                cursor++; line++;
-            }
-            if (input.charAt(cursor) == '\t') {
-                cursor++; column++;
-            }
-            if (input.charAt(cursor) == '\r') {
-                cursor++; column++;
-            }
-
-            if (input.charAt(cursor) == '(') {
+            if (c == '(') {
                 cursor++;
                 column++;
                 tokens.add(new Token(TokenType.LPAREN, "(", null, line, column));
-            }
-
-            if (input.charAt(cursor) == ')') {
+            } else if (c == ')') {
                 cursor++;
                 column++;
                 tokens.add(new Token(TokenType.RPAREN, ")", null, line, column));
-            }
-
+            } else if (Character.isDigit(c)) {
+                int startColumn = column;
+                StringBuilder number = new StringBuilder();
+                while (cursor < input.length() && Character.isDigit(input.charAt(cursor))) {
+                    number.append(input.charAt(cursor));
+                    cursor++;
+                    column++;
+                }
+                tokens.add(new Token(TokenType.NUMBER, number.toString(), Double.parseDouble(number.toString()), line, startColumn));
+            } else if (input.startsWith("plus", cursor)) {
+                cursor += 4;
+                column += 4;
+                tokens.add(new Token(TokenType.PLUS, "+", null, line, column));
+            } else if (input.startsWith("minus", cursor)) {
+                cursor += 5;
+                column += 5;
+                tokens.add(new Token(TokenType.MINUS, "-", null, line, column));
+            } else if (input.startsWith("times", cursor)) {
+                cursor += 5;
+                column += 5;
+                tokens.add(new Token(TokenType.TIMES, "*", null, line, column));
+            } else if (input.startsWith("divided by", cursor)) {
+                cursor += 10;
+                column += 10;
+                tokens.add(new Token(TokenType.DIVIDED_BY, "/", null, line, column));
+            } else
+                throw new RuntimeException("Unexpected character '" + c + "' at line " + line + ", column " + column);
         }
 
         return tokens;
     }
-
 }
